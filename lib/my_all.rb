d71367041b25458b878de6f907b7a0135a36f650
Anthony Leon <acidknights89@gmail.com>
2020-09-15 22:49:46 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,7 +5,11 @@ def my_all?(collection)
   block_return_values = []
   while i < collection.length 
   block_return_values << yield(collection[i])
+<<<<<<< HEAD
   i = i + 1 
+=======
+  i - i + 1 
+>>>>>>> 8cbc970d3deb2738c983fd272af7ac5ce3ae5eb2
   end
   
   if block_return_values.include?(false)
